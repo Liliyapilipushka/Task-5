e4752eb52b88d55b6591888f32414d216af2ee99
liliy <pwerwe@dfsdf.ksdf>
2023-06-15 14:22:06 +0300

    This task is ready

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,23 +8,49 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+    private static final String hql = """
+            SELECT c FROM Car c WHERE c.model = :model AND series = :series
+            """;
+
+    @Override
+    public User searchUserByCar(String model, int series) {
+        var query = sessionFactory.getCurrentSession().createQuery(hql, Car.class);
+        query.setParameter("model", model);
+        query.setParameter("series",series);
+        Optional<Car> car = query.uniqueResultOptional();
+        User resultUser = null;
+        if (car.isPresent()){
+            Car resultCar = car.get();
+            List<User> list = this.listUsers();
+            for (User u : list) {
+                if (u.getCar().equals(resultCar)) {
+                    resultUser = u;
+                }
+            }
+        } else {
+            System.err.println("CAR NOT FOUND");
+        }
+        return resultUser;
+    }
 
 }
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,69 @@
+package hiber.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "car_id")
+    private Long carId;
+    private String model;
+    private int series;
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Car() {
+    }
+
+    public Long getId() {
+        return carId;
+    }
+
+    public void setId(Long id) {
+        this.carId = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return series == car.series && Objects.equals(carId, car.carId) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carId, model, series);
+    }
+}
